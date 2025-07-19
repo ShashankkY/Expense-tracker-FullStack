376ebf0bc5f63c79eed5e4cd8a00591eb60d17b5
ShashankkY <shashankyogesh12@gmail.com>
2025-07-19 19:44:20 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+document.getElementById('signupForm').addEventListener('submit', async function (e) {
+      e.preventDefault();
+
+      const name = document.getElementById('name').value;
+      const email = document.getElementById('email').value;
+      const password = document.getElementById('password').value;
+      const errorMessage = document.getElementById('errorMessage');
+
+      try {
+        const response = await fetch('http://localhost:3000/user/signup', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ name, email, password })
+        });
+
+        if (!response.ok) {
+          throw new Error('Signup failed');
+        }
+
+        const data = await response.json();
+        alert('Signup successful!');
+        window.location.href = 'login.html';
+      } catch (error) {
+        errorMessage.textContent = 'Error: Network Error or Signup Failed';
+      }
+    });
